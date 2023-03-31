c726faf83f3a73bd7cb11c6d66c1df7151c14592
RichMan <kovalenko.richman@gmail.com>
2023-03-31 12:45:16 +0300

    create Configuration class with accessKey, secretKey and imageListUrl

---
+++ lib/configuration/configuration.dart
@@ -0,0 +1,5 @@
+class Configuration {
+  static const accessKey = '4ZFTlEJScJ7BXZ1tE3jlT1JMlIBHqQSkKyL26HESfao';
+  static const secretKey = 'FxSYXDsq19SBBefTRocWRxs3GnWp7yc3as_KCMaZKdA';
+  static const imageListUrl = 'https://api.unsplash.com/photos';
+}
