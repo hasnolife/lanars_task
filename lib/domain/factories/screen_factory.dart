c243bb396a6082f00c71dc3da2cf591d58ff4243
RichMan <kovalenko.richman@gmail.com>
2023-03-31 14:45:32 +0300

    Add ScreenFactory class which created BlocProviders

---
+++ lib/domain/factories/screen_factory.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lanars_task/domain/blocs/Images_list_bloc/images_list_bloc.dart';
+import 'package:lanars_task/domain/blocs/image_details_bloc/image_details_bloc.dart';
+import 'package:lanars_task/ui/pages/image_details_page.dart';
+import 'package:lanars_task/ui/pages/images_list_page.dart';
+
+class ScreenFactory {
+  Widget makeImageListPage() {
+    return BlocProvider<ImagesListBloc>(
+      create: (_) => ImagesListBloc(),
+      child: const ImagesListPage(),
+    );
+  }
+  Widget makeImageDetailsPage() {
+    return BlocProvider<ImageDetailsBloc>(
+      create: (_) => ImageDetailsBloc(),
+      child: const ImageDetailsPage(),
+    );
+  }
+
+}
