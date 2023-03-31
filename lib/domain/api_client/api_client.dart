58cc9d20ecb1557c73bb9b1d2ecc469e9c38efa8
RichMan <kovalenko.richman@gmail.com>
2023-03-31 15:42:05 +0300

    create method to get image details

--- lib/domain/api_client/api_client.dart
+++ lib/domain/api_client/api_client.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:lanars_task/configuration/configuration.dart';
+import 'package:lanars_task/domain/entity/image_details_model.dart';
 import 'package:lanars_task/domain/entity/image_model.dart';
 
 class ApiClient {
@@ -24,4 +25,26 @@ class ApiClient {
 
     return imagesList;
   }
+
+  Future<ImageDetailsModel> getImageDetails(String imageId) async {
+    final response = await _dio.get('${Configuration.imageListUrl}/$imageId',
+        queryParameters: {'client_id': Configuration.accessKey});
+
+    final data = response.data as Map<String, dynamic>;
+    final imageUrl = data["urls"]["full"];
+    final width = data["width"];
+    final height = data["height"];
+    final likes = data["likes"];
+    final description = data["description"];
+    final userName = data["user"]["name"];
+
+    return ImageDetailsModel(id: imageId,
+      imageUrl: imageUrl,
+      width: width,
+      height: height,
+      likes: likes,
+      description: description,
+      userName: userName,);
+  }
+
 }
