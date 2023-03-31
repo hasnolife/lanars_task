3247a4eeddf3912a3d8158ba921199a4f6a203d4
RichMan <kovalenko.richman@gmail.com>
2023-03-31 12:41:36 +0300

    create ImageModel with description and image url fields

---
+++ lib/domain/entity/image_model.dart
@@ -0,0 +1,11 @@
+class ImageModel {
+  final String? description;
+  final String smallImage;
+  final String fullImage;
+
+  const ImageModel({
+    required this.description,
+    required this.smallImage,
+    required this.fullImage,
+  });
+}
