b950aadbaf53787d49b34af8525fcf656d00f9d1
RichMan <kovalenko.richman@gmail.com>
2023-03-31 14:47:50 +0300

    change ImageModel entity

--- lib/domain/entity/image_model.dart
+++ lib/domain/entity/image_model.dart
@@ -1,11 +1,9 @@
 class ImageModel {
-  final String? description;
-  final String smallImage;
-  final String fullImage;
+  final String imageUrl;
+  final String id;
 
   const ImageModel({
-    required this.description,
-    required this.smallImage,
-    required this.fullImage,
+    required this.imageUrl,
+    required this.id,
   });
 }
