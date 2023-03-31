c0c2b86c11b0caf3bd3280c4e8036ecd5ae1c659
RichMan <kovalenko.richman@gmail.com>
2023-03-31 16:29:40 +0300

    add ImageDetailsModel data to ListView. Image wrap with InteractiveViewer for zooming from now on pull-down close this page

--- lib/ui/pages/image_details_page.dart
+++ lib/ui/pages/image_details_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lanars_task/domain/blocs/image_details_bloc/image_details_bloc.dart';
 
-
 class ImageDetailsPage extends StatelessWidget {
   const ImageDetailsPage({super.key});
 
@@ -14,7 +13,31 @@ class ImageDetailsPage extends StatelessWidget {
         bloc: bloc,
         builder: (BuildContext context, state) {
           if (state is DataState) {
-            return ListView();
+            final imageDetails = state.imageDetails;
+            return RefreshIndicator(
+              onRefresh: () async => Navigator.of(context).pop(),
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: ListView(
+                  children: [
+                    InteractiveViewer(
+                      boundaryMargin: const EdgeInsets.all(20.0),
+                      minScale: 0.1,
+                      maxScale: 3.0,
+                      child: Image.network(
+                        imageDetails.imageUrl,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    // Image.network(imageDetails.imageUrl),
+                    Text('Image author : ${imageDetails.userName}'),
+                    Text(
+                        'Image size :  ${imageDetails.width} x ${imageDetails.width}'),
+                    Text('Image has ${imageDetails.likes} likes'),
+                  ],
+                ),
+              ),
+            );
           }
           if (state is ErrorState) {
             return Container();
