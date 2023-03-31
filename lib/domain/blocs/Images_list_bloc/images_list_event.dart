2878ee33839042c755c5cb9dae2856e677c35700
RichMan <kovalenko.richman@gmail.com>
2023-03-31 15:07:33 +0300

    add LoadListEvent with completer

--- lib/domain/blocs/Images_list_bloc/images_list_event.dart
+++ lib/domain/blocs/Images_list_bloc/images_list_event.dart
@@ -3,4 +3,8 @@ part of 'images_list_bloc.dart';
 @immutable
 abstract class ImagesListEvent {}
 
-class LoadListEvent extends ImagesListEvent {}
+class LoadListEvent extends ImagesListEvent {
+  final Completer? completer;
+
+  LoadListEvent({this.completer});
+}
