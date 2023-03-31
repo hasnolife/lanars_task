82af9235d40c6a8fb38d18af83587fc478d1df14
RichMan <kovalenko.richman@gmail.com>
2023-03-31 16:11:08 +0300

    add LoadDetailsEvent to get details data

--- lib/domain/blocs/image_details_bloc/image_details_bloc.dart
+++ lib/domain/blocs/image_details_bloc/image_details_bloc.dart
@@ -1,15 +1,20 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lanars_task/domain/api_client/api_client.dart';
 import 'package:lanars_task/domain/entity/image_details_model.dart';
 
 part 'image_details_event.dart';
 part 'image_details_state.dart';
 
 class ImageDetailsBloc extends Bloc<ImageDetailsEvent, ImageDetailsState> {
-  ImageDetailsBloc() : super(EmptyState()) {
+  ImageDetailsBloc(this.imageId) : super(EmptyState()) {
     on<LoadDetailsEvent>((event, emit) async{
-
+      final imageDetails = await _apiClient.getImageDetails(imageId);
+      emit(DataState(imageDetails: imageDetails));
     });
+    add(LoadDetailsEvent(imageId: imageId));
   }
+  final _apiClient = ApiClient();
+  final String imageId;
 }
