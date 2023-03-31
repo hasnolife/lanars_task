d4c947c7f71bfc184d927cc58c928c1bd1b4359d
RichMan <kovalenko.richman@gmail.com>
2023-03-31 15:04:47 +0300

    add route transition on tap image

--- lib/ui/pages/images_list_page.dart
+++ lib/ui/pages/images_list_page.dart
@@ -3,13 +3,14 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lanars_task/domain/blocs/Images_list_bloc/images_list_bloc.dart';
+import 'package:lanars_task/ui/navigation/main_navigation.dart';
 
 class ImagesListPage extends StatelessWidget {
   const ImagesListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final bloc = ImagesListBloc();
+    final bloc = context.read<ImagesListBloc>();
     return Scaffold(
       body: RefreshIndicator(
         onRefresh: () async {
@@ -25,11 +26,14 @@ class ImagesListPage extends StatelessWidget {
                 itemCount: state.imagesList.length,
                 itemBuilder: (context, index) {
                   final imageData = state.imagesList[index];
-                 return ListTile(
-                   title: Image.network(imageData.imageUrl),
-                   // subtitle: Text(imageData.description ?? ''),
-
-                 );
+                  return ListTile(
+                      title: Image.network(imageData.imageUrl),
+                      onTap: () {
+                        Navigator.of(context).pushNamed(
+                          MainNavigationRouteNames.details,
+                          arguments: imageData.id,
+                        );
+                      });
                 },
               );
             }
