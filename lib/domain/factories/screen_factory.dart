ec491ac024f08037eb269eafad97be99d0a7ae0f
RichMan <kovalenko.richman@gmail.com>
2023-04-02 22:36:40 +0300

    ref: add service locator with GetIt and implement it

--- lib/domain/factories/screen_factory.dart
+++ lib/domain/factories/screen_factory.dart
@@ -2,21 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lanars_task/domain/blocs/Images_list_bloc/images_list_bloc.dart';
 import 'package:lanars_task/domain/blocs/image_details_bloc/image_details_bloc.dart';
+import 'package:lanars_task/service_locator.dart';
 import 'package:lanars_task/ui/pages/image_details_page.dart';
 import 'package:lanars_task/ui/pages/images_list_page.dart';
 
 class ScreenFactory {
   Widget makeImageListPage() {
     return BlocProvider<ImagesListBloc>(
-      create: (_) => ImagesListBloc(),
+      create: (_) => ImagesListBloc(apiClient: sl()),
       child: const ImagesListPage(),
     );
   }
+
   Widget makeImageDetailsPage(String imageId) {
     return BlocProvider<ImageDetailsBloc>(
-      create: (_) => ImageDetailsBloc(imageId),
+      create: (_) => ImageDetailsBloc(
+        imageId: imageId,
+        apiClient: sl(),
+      ),
       child: ImageDetailsPage(imageId: imageId),
     );
   }
-
 }
