24ba1bedb5d760b9e9a07ec7876587469092f88e
RichMan <kovalenko.richman@gmail.com>
2023-04-02 22:11:33 +0300

    ref: Configuration

--- lib/configuration/configuration.dart
+++ lib/configuration/configuration.dart
@@ -1,6 +1,7 @@
 class Configuration {
   static const accessKey = '4ZFTlEJScJ7BXZ1tE3jlT1JMlIBHqQSkKyL26HESfao';
   static const secretKey = 'FxSYXDsq19SBBefTRocWRxs3GnWp7yc3as_KCMaZKdA';
-  static const imageListUrl = 'https://api.unsplash.com/photos';
-  static const searchListUrl = 'https://api.unsplash.com/search/photos';
+  static const unsplashUrl = 'https://api.unsplash.com';
+  static const imageListHeader = '/photos';
+  static const searchHeader = '/search';
 }
