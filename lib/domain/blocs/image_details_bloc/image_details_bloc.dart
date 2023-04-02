8a499b122671514188fc2b6d5f2f663ef7c8d7e1
RichMan <kovalenko.richman@gmail.com>
2023-04-02 22:39:28 +0300

    ref: change RefreshIndicator on Dismissible and extract Widgets

--- lib/domain/blocs/image_details_bloc/image_details_bloc.dart
+++ lib/domain/blocs/image_details_bloc/image_details_bloc.dart
@@ -1,17 +1,17 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:lanars_task/domain/api_client/api_client.dart';
+import 'package:lanars_task/domain/api_client/image_details_api_client.dart';
 import 'package:lanars_task/domain/entity/image_details_model.dart';
 
 part 'image_details_event.dart';
 part 'image_details_state.dart';
 
 class ImageDetailsBloc extends Bloc<ImageDetailsEvent, ImageDetailsState> {
-  ImageDetailsBloc(this.imageId) : super(EmptyState()) {
+  ImageDetailsBloc({required this.imageId, required this.apiClient}) : super(EmptyState()) {
     on<LoadDetailsEvent>((event, emit) async{
       try {
-        final imageDetails = await _apiClient.getImageDetails(imageId);
+        final imageDetails = await apiClient.getImageDetails(imageId);
         emit(DataState(imageDetails: imageDetails));
       } catch (e) {
         emit(ErrorState(e));
@@ -19,6 +19,6 @@ class ImageDetailsBloc extends Bloc<ImageDetailsEvent, ImageDetailsState> {
     });
     add(LoadDetailsEvent(imageId: imageId));
   }
-  final _apiClient = ApiClient();
+  final ImageDetailsApiClient apiClient;
   final String imageId;
 }
--- lib/ui/pages/image_details_page.dart
+++ lib/ui/pages/image_details_page.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lanars_task/domain/blocs/image_details_bloc/image_details_bloc.dart';
 import 'package:lanars_task/ui/widgets/image_error_widget.dart';
+import 'package:lanars_task/ui/widgets/image_details_card.dart';
 
 class ImageDetailsPage extends StatelessWidget {
   const ImageDetailsPage({super.key, required this.imageId});
+
   final String imageId;
 
   @override
@@ -15,29 +17,15 @@ class ImageDetailsPage extends StatelessWidget {
         bloc: bloc,
         builder: (BuildContext context, state) {
           if (state is DataState) {
-            final imageDetails = state.imageDetails;
-            return RefreshIndicator(
-              onRefresh: () async => Navigator.of(context).pop(),
-              child: Padding(
-                padding: const EdgeInsets.all(10),
-                child: ListView(
-                  children: [
-                    InteractiveViewer(
-                      boundaryMargin: const EdgeInsets.all(20.0),
-                      minScale: 0.1,
-                      maxScale: 3.0,
-                      child: Image.network(
-                        imageDetails.imageUrl,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    // Image.network(imageDetails.imageUrl),
-                    Text('Image author : ${imageDetails.userName}'),
-                    Text(
-                        'Image size :  ${imageDetails.width} x ${imageDetails.width}'),
-                    Text('Image has ${imageDetails.likes} likes'),
-                  ],
-                ),
+            return Padding(
+              padding: const EdgeInsets.all(10),
+              child: Dismissible(
+                direction: DismissDirection.down,
+                onDismissed: (_) {
+                  Navigator.of(context).pop();
+                },
+                key: const Key('key'),
+                child: ImageDetailsCard(imageDetails: state.imageDetails),
               ),
             );
           }
